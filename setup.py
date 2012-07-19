cbb6edf616438ec5c3900cabb544ce9e5dd0d503
Aurélien Matouillot <a.matouillot@gmail.com>
2012-07-19 09:10:29 +0200

    Create project

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='pyramid_cms',
+      version='0.0',
+      description='pyramid_cms',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyramid_cms',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyramid_cms:main
+      [console_scripts]
+      initialize_pyramid_cms_db = pyramid_cms.scripts.initializedb:main
+      """,
+      )
+
